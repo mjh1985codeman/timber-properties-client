f28865baa84603bf70e0a4a5f2e75d074fc5be0f
Michael Hodges <Michael.Hodges1985@yahoo.com>
2023-05-11 23:00:20 -0500

    recreated react app to try and get front end deploymenet successful

---
+++ src/components/NavItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from 'react-bootstrap/Nav'
+
+export default function NavItem(props) {
+
+  return (
+    <>  
+    <li>
+        <Nav.Link href={props.to}><h4 className='nav-item'>{props.text}</h4></Nav.Link>
+    </li>
+    </>
+  )
+}
