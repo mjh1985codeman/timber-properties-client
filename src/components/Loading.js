3ab8b11116e1322d98225329f03b61df25b4857d
Michael Hodges <Michael.Hodges1985@yahoo.com>
2023-05-13 16:19:51 -0500

    fixed loading thing

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,7 +4,8 @@ const Loading = () => {
 
   return (
     <>
-    <div className='loading'>If this is the first time viewing this page it can take a while.  Please be patient.  Thank You!!!</div>
+    <h4 className="loading-blurb">If this is the first time viewing this page it can take a while.  Please be patient.  Thank You!!!</h4>
+    <div className='loading'></div>
     </>
   );
 };
