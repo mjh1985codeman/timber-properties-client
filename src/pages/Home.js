101df20eeb058d0f87d73ae18080654dc12f893c
Michael Hodges <Michael.Hodges1985@yahoo.com>
2023-06-03 23:35:58 -0500

    home page refactor / style change WIP

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Auth from '../helpers/auth';
 import Container from "react-bootstrap/Container";
 import Card from 'react-bootstrap/Card';
 import homPic from '../assets/cabin-in-the-woods.jpg';
 import mainLogo from '../assets/timber-prop-logo.png';
 
 import { useNavigate } from "react-router-dom";
+
 
 
 const Home = () => {
@@ -19,19 +21,15 @@ const Home = () => {
     <div className="home-quote">  
     <h2>"A place to kick your feet up."</h2>
     </div>     
-
-    <Card className="bg-dark text-white">
+      <div className='home-pic-content'>
       <Card.Img className="home-img" src={homPic} alt="A beautiful cabin in the snowy woods on a lake." />
-      <Card.ImgOverlay>
-        <div className='home-pic-content'>
-        <Card.Title><h2 className='pic-title'>About Timber Properties</h2></Card.Title>
-        <Card.Text className='pic-txt'>
-          We have the Properties that are perfect for your Vactation.
-        </Card.Text>
-        <div className="big-disclaimer home-disclaimer" onClick={() => {navigate('/login')}}>LogIn To Book your spot today!</div>
+        <Card.Title><h2 className='pic-title'>We have the Properties that are perfect for your next get away.</h2></Card.Title>
+        {Auth.loggedIn() ? (
+        null
+        ) : (
+          <div className="big-disclaimer home-disclaimer" onClick={() => {navigate('/login')}}>Login to book your reservation today!</div> 
+        )}
         </div>
-      </Card.ImgOverlay>
-    </Card>
     </Container>
   );
 };
