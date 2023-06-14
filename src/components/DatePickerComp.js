0ebf5e297a65aa1471ca857291aba0bb2581f05b
Michael Hodges <Michael.Hodges1985@Yahoo.com>
2023-06-14 15:14:27 -0500

    Update DatePickerComp.js

--- src/components/DatePickerComp.js
+++ src/components/DatePickerComp.js
@@ -39,6 +39,7 @@ export default function DatePickerComp({ unavailableDates, onDateSelect }) {
         excludeDates={disabledDates}
         minDate={new Date()}
         withPortal
+        disabledKeyboardNavigation
       />
     </>
   );
