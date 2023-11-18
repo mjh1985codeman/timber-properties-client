f3a046f8747b097d581a0ad05fe9a3b7845b7f3a
Michael Hodges <Michael.Hodges1985@yahoo.com>
2023-11-18 07:23:19 -0600

    made the navigation alot quicker and smoother

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,26 +1,23 @@
 import React from 'react'
-import Nav from 'react-bootstrap/Nav'
+//import Nav from 'react-bootstrap/Nav'
+import { Link, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 export default function NavItem(props) {
+  const location = useLocation();
   const [isActive, setIsActive] = useState(false);
 
-// Check if the current component matches the selected navigation item
-// and set the "isActive" state accordingly
-useEffect(() => {
-  const currentPath = window.location.pathname;
-  setIsActive(currentPath === props.to);
-}, [props.to]);
+ // Check if the current location matches the selected navigation item
+  // and set the "isActive" state accordingly
+  useEffect(() => {
+    setIsActive(location.pathname === props.to);
+  }, [location.pathname, props.to]);
 
   return (
     <> 
-    
-        <Nav.Link href={props.to}>
-        <div className={isActive ? 'nav-it-group active' : 'nav-it-group'}>
+    <Link to={props.to} className={isActive ? 'nav-it-group active' : 'nav-it-group'}>
         <h1 className='icon'>{props.icon}</h1>
-        </div>
-        </Nav.Link>
-    
+    </Link>
     </>
   )
 }
